86a925bfed10b83912ca233c5b816456b55def6d
Joel Terrell <falconreid@hotmail.com>
2020-07-16 10:03:00 -0600

    added basic timer function and section switching

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
-var totalSum = function (num) {
-  var sum = 0;
-  for (var i = 0; i <= num; i++) {
-    //  sum += i;
-  }
-  // return sum;
-};
+// adding variables
 
-console.log(i);
+// Quiz section
+var quizSection = document.querySelector("#quizContent");
+var questionSec = document.querySelector("#boldQuest");
+var ulAnswers = document.querySelector("#ulAnswers");
+var timeEl = document.querySelector("#timeLeft");
+
+// Final Section
+var finalSection = document.querySelector("#finalScore");
+var entryForm = document.querySelector("#initialsForm"); // to create entry form
+var ulInitials = document.querySelector("#scoreInitials");
+var scoreEntry = document.querySelector("#entry");
+
+// timer function
+var secondsLeft = 10;
+
+function setTime() {
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
+    timeEl.textContent = "Time Remaining: " + secondsLeft;
+
+    if (secondsLeft === 0) {
+      clearInterval(timerInterval);
+      sectionSwitch();
+    }
+  }, 1000);
+}
+
+function sectionSwitch() {
+  quizSection.setAttribute("style", "display: none;");
+  finalSection.setAttribute("style", "display: block;");
+}
+
+setTime();
+// function to return to quiz after user enters initials
+function sectionSwitchBack() {
+  quizSection.setAttribute("style", "display: block;");
+  finalSection.setAttribute("style", "display: none;");
+}
