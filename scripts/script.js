4ddc0a9e127a923be5b278243463994131819d6b
Joel Terrell <falconreid@hotmail.com>
2020-07-18 19:17:30 -0600

    created data attributes to link correct answer to click event

--- scripts/script.js
+++ scripts/script.js
@@ -4,20 +4,22 @@ var quizSection = document.querySelector("#quizContent");
 var questionSec = document.querySelector("#question");
 var ulAnswers = document.querySelector("#ulAnswers");
 var timeEl = document.querySelector("#timeLeft");
-var score = document.querySelector("#currentScore");
+var scoreCurrent = document.querySelector("#currentScore");
 var liEl = document.getElementsByClassName("answers");
+var score = "0";
 
 // Set of questions
 var questionList = [
   {
     question: "What is the the definition of a function?",
     choice: [
-      "Functions let you group together a series of statements to perform a specific task.",
+      "Functions are often used to loop through the items in an array.",
       "Functions let you combine arrays and objects to create complex data structures.",
       "Functions allow you to compare the results of one or more comparison operators.",
-      "Functions are often used to loop through the items in an array.",
+      "Functions let you group together a series of statements to perform a specific task.",
     ],
-    answer: 0,
+    answer:
+      "Functions let you group together a series of statements to perform a specific task.",
   },
   {
     question:
@@ -28,17 +30,18 @@ var questionList = [
       "Whether the variable is an array or a string.",
       "Whether or not the data type is Boolean or Undefined.",
     ],
-    answer: 1,
+    answer: "Whether or not the condition is true or false.",
   },
   {
-    question: "What is the the definition of a function?",
+    question:
+      "Which of the following are all used to access and update the DOM tree?",
     choice: [
       "offerExpire(), listArray(), checkUserName(), dismissNotation()",
       "charCount(), checkTerms(), removeAttr(), eventType()",
       "backgroundPosition(), wordSpacing(), isNumeric(), newItemButton()",
       "getElementByID(), nextSibling, querySelector(), parentNode",
     ],
-    answer: 3,
+    answer: "getElementByID(), nextSibling, querySelector(), parentNode",
   },
   {
     question: "How does a FOR Loop start?",
@@ -48,19 +51,22 @@ var questionList = [
       "for (i = 0; i <= 5; i++)",
       "for (i <= 5; i++)",
     ],
-    answer: 2,
+    answer: "for (i = 0; i <= 5; i++)",
   },
 ];
 
 // timer function
-var secondsLeft = 25 * questionList.length;
+var secondsLeft = 15 * questionList.length;
 
 function setTime() {
   var timerInterval = setInterval(function () {
     secondsLeft--;
     timeEl.textContent = "Time Remaining: " + secondsLeft;
+    scoreCurrent.textContent =
+      "Current Score: " + score + "/" + questionList.length;
 
     if (secondsLeft === 0) {
+      timeEl.textContent = "Time Is Up!";
       clearInterval(timerInterval);
       sectionSwitch();
     }
@@ -69,15 +75,19 @@ function setTime() {
 
 // function to get questions
 function getQuestion() {
-  currentQuestion++;
+  currentQuestion;
   document.getElementById("question").innerHTML =
     questionList[currentQuestion].question;
+
   document.getElementById("a1").innerHTML =
     questionList[currentQuestion].choice[0];
+
   document.getElementById("a2").innerHTML =
     questionList[currentQuestion].choice[1];
+
   document.getElementById("a3").innerHTML =
     questionList[currentQuestion].choice[2];
+
   document.getElementById("a4").innerHTML =
     questionList[currentQuestion].choice[3];
 }
@@ -95,8 +105,22 @@ document.getElementById("button").addEventListener("click", function () {
 
 // get next question
 for (var i = 0; i < liEl.length; i++) {
-  liEl[i].addEventListener("click", function () {
-    console.log(getQuestion());
+  liEl[i].addEventListener("click", function (e) {
+    console.log(e.target.textContent);
+    console.log(questionList[currentQuestion].answer);
+    if (e.target.textContent === questionList[currentQuestion].answer) {
+      console.log("correct");
+      score.textContent = score++;
+    } else {
+      console.log("WRONG!");
+    }
+    currentQuestion++;
+    getQuestion();
+
+    if (liEl.length > 3) {
+      clearInterval(timerInterval);
+      sectionSwitch();
+    }
   });
 }
 
